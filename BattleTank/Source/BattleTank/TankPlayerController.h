12e395cfa8da2c0aa8630931ba5b845bf782f25b
ILATIQUA\Spencer <spencerjamesbarrett@gmail.com>
2017-04-12 14:55:14 -0500

    4/12/17 Ray trace through cross hair and collect HitLocation data.

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -36,7 +36,11 @@ private:
 	UPROPERTY(EditAnywhere)
 		float CrosshairYLocation = 0.33333f;
 
+	UPROPERTY(EditAnywhere)
+		float LineTraceRange = 1000000.0;
+
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
 	
 	
 };
